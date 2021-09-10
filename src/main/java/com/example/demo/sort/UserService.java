3d6c0acf064022ccf46a4ca240def20a17a1e7b8
SuperVanush <supervanush@yandex.ru>
2021-09-10 13:35:22 +0300

    сделала отдельный класс - не переименовывала пока методы

--- src/main/java/com/example/demo/sort/UserService.java
+++ src/main/java/com/example/demo/sort/UserService.java
@@ -3,6 +3,8 @@ package com.example.demo.sort;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UserService {
     SortUsers sortUsers = new SortUsers();
@@ -13,20 +15,28 @@ public class UserService {
         String choiceAddUser;
         String userName;
         int userId;
-
         do {
             System.out.println("Input name of user");
             userName = in.next();
-            System.out.println("Input ID of user");
-            userId = in.nextInt();
-            System.out.println("Do you want add next user? y/n");
-            choiceAddUser = in.next();
-            userList.add(new com.example.demo.sort.User(userId, userName));
+            for (int elem = 0; elem <= userName.length(); elem++) {
+                String alphabet = "^[a-zA-Z]*$";
+                Pattern pattern = Pattern.compile(alphabet);
+                Matcher matcher = pattern.matcher(userName);
+                if (!matcher.matches()) {
+                    System.out.println("This is not text. Try again");
+                    startApp();
+                }}
+                System.out.println("Input ID of user");
+                userId = in.nextInt();
+
+                System.out.println("Do you want add next user? y/n");
+                choiceAddUser = in.next();
+                userList.add(new com.example.demo.sort.User(userId, userName));
+            }
+            while (choiceAddUser.equals("y")) ;
+            {
+                sortUsers.startTwo(userList);
+            }}
         }
-        while (choiceAddUser.equals("y"));
 
-        sortUsers.startTwo(userList);
-    }
-
-}
 
