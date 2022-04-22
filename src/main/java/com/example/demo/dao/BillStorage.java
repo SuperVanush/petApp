8a1bcd48bf4a63805de01ed2c4a33551cc475516
SuperVanush <supervanush@yandex.ru>
2022-04-22 15:31:16 +0300

    и поправила bill снова. видимл слетело

--- src/main/java/com/example/demo/dao/BillStorage.java
+++ src/main/java/com/example/demo/dao/BillStorage.java
@@ -1,5 +1,6 @@
 package com.example.demo.dao;
 
+import com.example.demo.exception.BillListException;
 import com.example.demo.exception.UserListException;
 import com.example.demo.model.Bill;
 import com.example.demo.model.User;
@@ -30,17 +31,17 @@ public class BillStorage implements Storage<Bill> {
     }
 
     @Override
-    public Bill findById(int id) throws UserListException {
+    public Bill findById(int id) throws BillListException {
         for (Bill billInList : biilList) {
             if (billInList.getId() == id) {
                 return billInList;
             }
         }
-        throw new UserListException("User is not found");
+        throw new BillListException("Bill is not found");
     }
 
     @Override
-    public void remove(int id) throws UserListException {
+    public void remove(int id) throws BillListException {
 
     }
 
@@ -51,6 +52,6 @@ public class BillStorage implements Storage<Bill> {
 
     @Override
     public List<Bill> getListOfElements() {
-        return null;
+        return biilList;
     }
 }
