979931163f6930f027613ccbbd48718088e05663
SuperVanush <supervanush@yandex.ru>
2022-10-14 17:16:37 +0300

    создала новую ветку, создала файл по логированию, дальше пока не делала

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
 import com.example.demo.view.StartProgram;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 
 public class Main {
 
     public static void main(String[] args) {
         StartProgram startProgram = new StartProgram();
+        Logger.getRootLogger().setLevel(Level.OFF);
         startProgram.startApp();
     }
 }
