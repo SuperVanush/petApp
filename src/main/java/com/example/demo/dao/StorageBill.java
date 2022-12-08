0a6acd33aaa292b42a13ae3c5c6a296880cb4db7
SuperVanush <supervanush@yandex.ru>
2022-12-08 14:56:42 +0300

    занесла в зависимости Spring и DaoFactory поправила (удалила). теперь не пойму как настроить коннект с БД

--- src/main/java/com/example/demo/dao/StorageBill.java
+++ src/main/java/com/example/demo/dao/StorageBill.java
@@ -1,10 +1,9 @@
 package com.example.demo.dao;
 
 import com.example.demo.model.Bill;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
-@Service
+
 public interface StorageBill {
 
     Bill add(Bill bill);
