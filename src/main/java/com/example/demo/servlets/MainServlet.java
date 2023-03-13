46164de4248f5b3ddf341ffea89fb63494ad278c
SuperVanush <supervanush@yandex.ru>
2023-03-13 15:09:12 +0300

    все что могла понатыкала. пока не буду трогать ))

--- src/main/java/com/example/demo/servlets/MainServlet.java
+++ src/main/java/com/example/demo/servlets/MainServlet.java
@@ -1,6 +1,9 @@
 package com.example.demo.servlets;
 
-import org.springframework.stereotype.Service;
+import com.example.demo.service.ServiceConfiguration;
+import com.example.demo.service.impl.UserService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,12 +11,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-@Service
+
 public class MainServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(ServiceConfiguration.class);
+        context.getBean(UserService.class);
 
         PrintWriter writer = response.getWriter();
         Integer userId = (Integer) request.getSession().getAttribute("userId");
--- src/main/java/com/example/demo/view/ViewConfig.java
+++ src/main/java/com/example/demo/view/ViewConfig.java
@@ -1,13 +1,9 @@
 package com.example.demo.view;
 
-import com.example.demo.dao.DaoConfiguration;
-import com.example.demo.service.ServiceConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @ComponentScan
-@Import({ServiceConfiguration.class, DaoConfiguration.class})
 @Configuration
 public class ViewConfig {
 }
